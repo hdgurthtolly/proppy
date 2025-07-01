afc820c719c66d33aad326d8c592afaa6e260964
Harry Daniel Gurth Angeles <hdgurth@gmail.com>
2025-07-01 12:41:04 +0200

    Set-up Sanity

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,5 +3,12 @@ export default function RootLayout({
 }: {
   children: React.ReactNode
 }) {
-  return children
+  return (
+    <html lang="en">
+      <head />
+      <body>
+        {children}
+      </body>
+    </html>
+  )
 } 
